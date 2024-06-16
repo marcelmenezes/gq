92f555f6676979a51c09bc774d58891d635996db
marcelmenezes <marcel.business@gmail.com>
2024-06-16 13:22:41 -0300

    Groq teste 1

---
+++ teste.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+groq_api_key = os.getenv("GROQ_API_KEY")
+
+client = Groq(
+    api_key=groq_api_key,
+)
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "are you multimodal?",
+        }
+    ],
+    model="llama3-70b-8192",
+)
+print(chat_completion.choices[0].message.content)
